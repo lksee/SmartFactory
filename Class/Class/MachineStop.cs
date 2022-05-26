f9594f08382b03da31d3b339907524d1158accb6
biana-bdwrtr <bdwrtr@naver.com>
2022-05-26 11:36:09 +0900

    class, instance, static에 대한 복습

---
+++ Class/Class/MachineStop.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Class
+{
+    public partial class MachineStop : Form
+    {
+        public MachineStop()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("중지 전 기계 상태: " + MachineRunStopFlag.sRunStopFlag);
+
+            MachineRunStopFlag.sRunStopFlag = "대기";
+
+            MessageBox.Show("중지 후 기계 상태: " + MachineRunStopFlag.sRunStopFlag);
+        }
+    }
+}
